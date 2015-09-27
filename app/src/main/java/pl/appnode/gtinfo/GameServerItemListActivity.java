ed63b85f1b06ba69972d4669cc6eac5b863f49b0
WojtekM <wojciech.mardyla@gmail.com>
2015-09-27 13:36:27 +0100

    Main menu in servers list (starting about dialog).

--- app/src/main/java/pl/appnode/gtinfo/GameServerItemListActivity.java
+++ app/src/main/java/pl/appnode/gtinfo/GameServerItemListActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 /**
@@ -56,6 +58,25 @@ public class GameServerItemListActivity extends AppCompatActivity
         }
 
         // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            AboutDialog.showDialog(GameServerItemListActivity.this);
+        }
+        if (id == R.id.action_settings) {
+//            Intent settingsIntent = new Intent(this, PreferencesActivity.class);
+//            this.startActivity(settingsIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
