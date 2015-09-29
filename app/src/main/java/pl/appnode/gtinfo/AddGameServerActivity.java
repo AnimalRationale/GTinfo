238caaa4eabcab37dce12ca2cf046e05567cb860
WojtekM <wojciech.mardyla@gmail.com>
2015-09-29 23:49:16 +0100

    Init for click listeners in add server dialog activity.

--- app/src/main/java/pl/appnode/gtinfo/AddGameServerActivity.java
+++ app/src/main/java/pl/appnode/gtinfo/AddGameServerActivity.java
@@ -2,13 +2,18 @@ package pl.appnode.gtinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import static pl.appnode.gtinfo.PreferencesSetupHelper.themeSetup;
 
-public class AddGameServerActivity extends Activity {
+public class AddGameServerActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "AddServer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,29 @@ public class AddGameServerActivity extends Activity {
         getWindow().setAttributes(layoutParams);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         this.setFinishOnTouchOutside(false);
+        Button buttonOk = (Button) findViewById(R.id.okAddServer);
+        buttonOk.setOnClickListener(this);
+        Button buttonCancel = (Button) findViewById(R.id.cancelAddServer);
+        buttonCancel.setOnClickListener(this);
+    }
+
+    private void pressedOk() {
+        finish();
+    }
+
+    private void pressedCancel() {
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.okAddServer:
+                pressedOk();
+                break;
+            case R.id.cancelAddServer:
+                pressedCancel();
+                break;
+        }
     }
 }
