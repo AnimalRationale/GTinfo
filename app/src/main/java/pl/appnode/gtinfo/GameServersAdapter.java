4da01d3a642d9d6d0f485d5974cb36a72adc4020
WojtekM <wojciech.mardyla@gmail.com>
2015-10-05 23:51:45 +0100

    Init for card/layout elements on-click listener in RrecyclerView.

--- app/src/main/java/pl/appnode/gtinfo/GameServersAdapter.java
+++ app/src/main/java/pl/appnode/gtinfo/GameServersAdapter.java
@@ -1,6 +1,7 @@
 package pl.appnode.gtinfo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -32,6 +33,7 @@ public class GameServersAdapter extends RecyclerView.Adapter<GameServersAdapter.
         final GameServerItem gameServer = sServersList.get(position);
         ServerViewHolder.vName.setText(gameServer.mName);
         ServerViewHolder.vAddress.setText(gameServer.mId);
+        ServerViewHolder.vPosition = position;
     }
 
     @Override
@@ -39,22 +41,47 @@ public class GameServersAdapter extends RecyclerView.Adapter<GameServersAdapter.
         View itemView = LayoutInflater.
                 from(viewGroup.getContext()).
                 inflate(R.layout.card_layout, viewGroup, false);
+        GameServersAdapter.ServerViewHolder viewHolder = new ServerViewHolder
+                (itemView, new GameServersAdapter.ServerViewHolder.IViewHolderOnClicks() {
+            public void onCardClick(View caller) {
+                Intent detailIntent = new Intent(mContext, GameServerItemDetailActivity.class);
+                detailIntent.putExtra(GameServerItemDetailFragment.ARG_ITEM_ID, ServerViewHolder.vPosition);
+                mContext.startActivity(detailIntent);
+            }
+                });
         CardView card = (CardView) itemView;
         if (isDarkTheme(mContext)) {
             card.setCardBackgroundColor(Color.BLACK);
         } else card.setCardBackgroundColor(Color.WHITE);
-        return new ServerViewHolder(itemView);
+        return viewHolder;
     }
 
-    public static class ServerViewHolder extends RecyclerView.ViewHolder {
+    public static class ServerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        protected IViewHolderOnClicks mClickListener;
+        protected static int vPosition;
         protected static TextView vName;
         protected static TextView vAddress;
 
-        public ServerViewHolder(View v) {
+        public ServerViewHolder(View v, IViewHolderOnClicks listener) {
             super(v);
             vName = (TextView) v.findViewById(R.id.server_name);
             vAddress = (TextView) v.findViewById(R.id.server_address);
+            vName.setOnClickListener(this);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v instanceof TextView) {
+                mClickListener.onCardClick(v);
+            } else {
+                mClickListener.onCardClick(v);
+            }
+        }
+
+        public interface IViewHolderOnClicks {
+            void onCardClick(View caller);
         }
     }
 }
