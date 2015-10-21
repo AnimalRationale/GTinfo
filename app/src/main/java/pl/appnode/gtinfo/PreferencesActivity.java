d3f0e44a97417ed4e2ffb010ffcc5c38881fbf91
WojtekM <wojciech.mardyla@gmail.com>
2015-10-21 20:56:05 +0100

    Server add debug tool menu in last position of main menu, preferences in fragment.

--- app/src/main/java/pl/appnode/gtinfo/PreferencesActivity.java
+++ app/src/main/java/pl/appnode/gtinfo/PreferencesActivity.java
@@ -1,15 +1,22 @@
 package pl.appnode.gtinfo;
 
 import android.app.ActionBar;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 import static pl.appnode.gtinfo.PreferencesSetupHelper.orientationSetup;
 import static pl.appnode.gtinfo.PreferencesSetupHelper.themeSetup;
 
-public class PreferencesActivity extends PreferenceActivity {
+public class PreferencesActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    public static final String KEY_PREF_THEME = "settings_checkbox_theme";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         themeSetup(this);
@@ -18,7 +25,25 @@ public class PreferencesActivity extends PreferenceActivity {
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_launcher);
-        addPreferencesFromResource(R.xml.activity_preferences);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new GTinfoPreferenceFragment()).commit();
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        if (key.equals(KEY_PREF_THEME)) {
+
+        }
+    }
+
+    public static class GTinfoPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.activity_preferences);
+        }
     }
 
     @Override
