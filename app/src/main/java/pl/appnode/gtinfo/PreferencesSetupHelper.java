6cf9fbaf0e6c1664fb4652ddc91931735e076c4b
AnimalRationale <wojciech.mardyla@gmail.com>
2015-11-16 21:06:52 +0000

    Cleanup in PreferncesSetupHelper.

--- app/src/main/java/pl/appnode/gtinfo/PreferencesSetupHelper.java
+++ app/src/main/java/pl/appnode/gtinfo/PreferencesSetupHelper.java
@@ -11,9 +11,10 @@ import static pl.appnode.gtinfo.Constants.KEY_SETTINGS_GT_SHOW_MAP;
 import static pl.appnode.gtinfo.Constants.KEY_SETTINGS_GT_SHOW_TOP_PLAYERS;
 import static pl.appnode.gtinfo.Constants.KEY_SETTINGS_THEME;
 
-
+/** Class with functions for reading and using application settings */
 public class PreferencesSetupHelper {
 
+    /** Sets up proper (dark or light) system theme */
     public static void themeSetup(Context context) {
 
         if (isDarkTheme(context)) {
@@ -29,23 +30,26 @@ public class PreferencesSetupHelper {
         }
     }
 
+    /** Controls ability to change app display orientation accordingly to device state */
     public static void orientationSetup(Activity activity) {
-
         if (isRotationOn(activity)) {
             activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
         } else activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    /** Returns state of dark theme setting in app preferences, used to set proper system theme */
     public static boolean isDarkTheme(Context context) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         return settings.getBoolean(KEY_SETTINGS_THEME, false);
     }
 
+    /** Returns state of preference setting allowing app display orientation change */
     public static boolean isRotationOn(Context context) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         return settings.getBoolean("settings_checkbox_orientation", false);
     }
 
+    /** Checks if app is started first time after installation */
     public static boolean isFirstRun(Context context) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         boolean firstRun = settings.getBoolean("settings_first_run", true);
@@ -53,11 +57,15 @@ public class PreferencesSetupHelper {
         return firstRun;
     }
 
+    /** Returns state of user preference setting controlling showing image
+     * of current map in HTML game info component  */
     public static boolean isShowMap(Context context) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         return settings.getBoolean(KEY_SETTINGS_GT_SHOW_MAP, false);
     }
 
+    /** Returns state of user preference setting controlling showing list of server top players
+     * in HTML game info component  */
     public static boolean isShowTopPlayers(Context context) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         return settings.getBoolean(KEY_SETTINGS_GT_SHOW_TOP_PLAYERS, false);
